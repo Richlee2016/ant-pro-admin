f61df76fa0e419dad59a45a9f6b6fd24c056df8f
Richlee2016 <345642459@qq.com>
2018-02-01 18:17:57 +0800

    0201

---
+++ src/routes/Movie/Online/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class MovieHot extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+                <span>321</span>
+            </div>
+        )
+    }
+}
